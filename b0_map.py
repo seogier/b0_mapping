02bb7ea623e51e55ee3b806727657095ff487b1c
Ogier <seo@NIST.GOV>
2026-07-06 14:03:22 -0600

    July 2026 Update

--- b0_map.py
+++ b0_map.py
@@ -116,6 +116,7 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser('Map the B0 of a magnet with a 3D printer and Hall probe.')
     parser.add_argument('probe_port', action='store', help='Port for Hall probe communication (e.g., COM1)')
     parser.add_argument('printer_port', action='store', help='Port for 3D printer communication (e.g., COM5)')
+    parser.add_argument('-n', '--init_only', action='store_true', help='Initialize 3D printer and exit')
     parser.add_argument('shape', type=str, action='store', choices=['cube', 'cylinder', 'sphere', 'circle'], help='Shape of sampling pattern', default='sphere')
     parser.add_argument('-d', '--diameter', action='store', default=100, help='Diameter of measurement volume (and length of cylinder if applicable) in mm')
     parser.add_argument('-x', '--set_center_pos', action='store_true', help='Use current position as center of measurement volume')
@@ -128,6 +129,7 @@ if __name__ == '__main__':
     args = parser.parse_args()
     port_lakeshore = args.probe_port
     port_printer = args.printer_port
+    init_only = args.init_only
     shape = args.shape
     diameter = args.diameter
     set_center_pos = args.set_center_pos
@@ -141,76 +143,78 @@ if __name__ == '__main__':
 
     printer = print3d.Printer(port_printer)
 
+    if not init_only:
 
-    if start != 0:
-        print(f'Starting at point {start}')
+        if start != 0:
+            print(f'Starting at point {start}')
 
-    home = input("Home printer?[y/n]")
-    if home.lower() == 'y':
-        print('Homing Printer, please wait.')
-        printer.home()
-        printer.wait()
-    
-    if set_center_pos:
-        input("Move field probe to magnet center and press Enter.")
-        center = printer.get_pos()
-
-    print(f'Generating a {shape} of diameter {diameter} mm centered about ({center[0]},{center[1]},{center[2]})')
-    print(f'Points are spaced every {spacing} mm')
-
-    # Define affine transformation to move from magnet to printer coordinate system
-    printer.affine =  np.array([[0,  0,  1, center[0]],
-                                [1,  0,  0, center[1]],
-                                [0, -1,  0, center[2]],
-                                [0,  0,  0,  0]])
-                
-    if shape == 'cube':
-        points = cube_gen(diameter, spacing)
-    elif shape == 'cylinder':
-        points = cylinder_gen(diameter, spacing)
-    elif shape == 'sphere':
-        points = sphere_gen(diameter, spacing)
-    elif shape == 'circle':
-        points = circle_gen(diameter, spacing)
-
-
-    n_points = points.shape[0]
-    with open(save_name, 'w', newline='') as csvfile:
-        csvwriter = csv.writer(csvfile, dialect='excel')
-        if probe.axes == 1:
-            csvwriter.writerow(['X', 'Y', 'Z', 'Bz', 'T'])
-        else:
-            csvwriter.writerow(['X', 'Y', 'Z', 'Mag', 'Bx', 'By', 'Bz', 'T'])
+        home = input("Home printer?[y/n]")
+        if home.lower() == 'y':
+            print('Homing Printer, please wait.')
+            printer.home()
+            printer.wait()
         
-        with alive_bar(n_points, dual_line=True) as bar:
-            def measure(point):
-                printer.move_mag(point)
-                time.sleep(0.5)
-                field = probe.get_field()
-                temp = probe.get_temp()
-                if probe.axes == 1:    
-                    bar.text(f'{point}: {field}')
-                    csvwriter.writerow([*point, field, temp])
-                else:
-                    bar.text(f'{point}: Mag:{field[0]}, X:{field[1]}, Y:{field[2]}, Z:{field[3]}')
-                    csvwriter.writerow([*point, *field, temp])
+        if set_center_pos:
+            input("Move field probe to magnet center and press Enter.")
+            center = printer.get_pos()
+
+        print(f'Generating a {shape} of diameter {diameter} mm centered about ({center[0]},{center[1]},{center[2]})')
+        print(f'Points are spaced every {spacing} mm')
+
+        # Define affine transformation to move from magnet to printer coordinate system
+        printer.affine =  np.array([[0,  0,  1, center[0]],
+                                    [1,  0,  0, center[1]],
+                                    [0, -1,  0, center[2]],
+                                    [0,  0,  0,  0]])
+                    
+        if shape == 'cube':
+            points = cube_gen(diameter, spacing)
+        elif shape == 'cylinder':
+            points = cylinder_gen(diameter, spacing)
+        elif shape == 'sphere':
+            points = sphere_gen(diameter, spacing)
+        elif shape == 'circle':
+            points = circle_gen(diameter, spacing)
 
 
-            bar.title('B0 Mapping in Progress')
-            if start !=0:
-                bar(start, skipped=True)
+        n_points = points.shape[0]
+        with open(save_name, 'w', newline='') as csvfile:
+            csvwriter = csv.writer(csvfile, dialect='excel')
+            if probe.axes == 1:
+                csvwriter.writerow(['X', 'Y', 'Z', 'Bz', 'T'])
+            else:
+                csvwriter.writerow(['X', 'Y', 'Z', 'Mag', 'Bx', 'By', 'Bz', 'T'])
+            
+            with alive_bar(n_points, dual_line=True) as bar:
+                def measure(point):
+                    printer.move_mag(point)
+                    time.sleep(0.5)
+                    field = probe.get_field()
+                    temp = probe.get_temp()
+                    if probe.axes == 1:    
+                        bar.text(f'{point}: {field}')
+                        csvwriter.writerow([*point, field, temp])
+                    else:
+                        bar.text(f'{point}: Mag:{field[0]}, X:{field[1]}, Y:{field[2]}, Z:{field[3]}')
+                        csvwriter.writerow([*point, *field, temp])
 
-            # measure center to start
-            measure([0,0,0])
 
-            for i in range(start, n_points):
-                # Re-measure center to track B0 drift
-                if i % remeasure_interval == 0:
-                    measure([0,0,0])
-                
-                point = points[i,:]
-                measure(point)
-                bar()
-            # Re-measure center to end
-            measure([0,0,0])
+                bar.title('B0 Mapping in Progress')
+                if start !=0:
+                    bar(start, skipped=True)
+
+                # measure center to start
+                measure([0,0,0])
+
+                for i in range(start, n_points):
+                    # Re-measure center to track B0 drift
+                    if i % remeasure_interval == 0:
+                        measure([0,0,0])
+                    
+                    point = points[i,:]
+                    measure(point)
+                    bar()
+                # Re-measure center to end
+                measure([0,0,0])
+
     printer.beep() # Let everyone know you're done!
--- b0_time.py
+++ b0_time.py
@@ -20,25 +20,17 @@ save_name = args.save_name
 
 probe = lakeshore.LakeshoreF71(port_lakeshore)
 
-printer = print3d.Printer(port_printer)
-
-home = input("Home printer?[y/n]")
-if home.lower() == 'y':
-    print('Homing Printer, please wait.')
-    printer.home()
-    printer.wait()
-
-n_points = 60*60*12
+n_points = 60*60*24
 with open(save_name, 'w', newline='') as csvfile:
     csvwriter = csv.writer(csvfile, dialect='excel')
-    csvwriter.writerow(['Time', 'Bz'])
+    csvwriter.writerow(['Time', 'Mag', 'Bx', 'By', 'Bz', 'T'])
     with alive_bar(n_points, dual_line=True) as bar:
         bar.title('B0 Stability Measurement in Progress')
         for point in range(n_points):
             time.sleep(1)
             timepoint = datetime.now()
             field = probe.get_field()
-            bar.text(f'{timepoint}: {field}')
-            csvwriter.writerow([timepoint,field])
+            temp = probe.get_temp()
+            bar.text(f'{timepoint}: Mag:{field[0]}, X:{field[1]}, Y:{field[2]}, Z:{field[3]}')
+            csvwriter.writerow([timepoint, *field, temp])
             bar()
-printer.beep() # Let everyone know you're done!
